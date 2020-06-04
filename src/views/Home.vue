3ac3b06e5a5f16726a7694b83de9330fcabb51f1
Maner <antonin.huaut+github@gmail.com>
2020-06-04 23:00:22 +0200

    removal of the warning about the new version

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="home">
     <SelectTimetable />
-
-    <div align="center">
-      <v-alert
-        outlined
-        type="warning"
-        max-width="950"
-        elevation="2"
-      >Les synchronisations, effectuées avec l'ancienne version de l'application, ne fonctionnent plus. Elles doivent être recréées.</v-alert>
-    </div>
   </div>
 </template>
 

b039dc7673605d153257a40a6543b7aa91ec3416
Antonin Huaut <me@antoninhuaut.fr>
2023-09-04 12:34:49 +0200

    Hotfix SelectTimetable view for 3A + openssl legacy

--- src/components/SelectTimetable.vue
+++ src/components/SelectTimetable.vue
@@ -6,35 +6,21 @@
         <v-btn text @click="snackbar = false;">Fermer</v-btn>
       </v-snackbar>
 
-      <v-col
-        class="mx-auto mb-5"
-        :cols="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? '': '6'"
-      >
+      <v-col class="mx-auto mb-5" :cols="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? '' : '6'">
         <span class="display-1">Veuillez sélectionner un emploi du temps :</span>
 
         <v-divider class="mt-3"></v-divider>
 
         <v-col class="mx-auto">
-          <h2 class="mt-3 mb-1">{{nameUniv}}</h2>
+          <h2 class="mt-3 mb-1">{{ nameUniv }}</h2>
           <v-row justify="center">
-            <v-col cols="6" class="mx-auto" v-for="(year) in ttsInfo.yearList" :key="year">
-              <h2 class="font-weight-regular">Année {{year}} :</h2>
-              <v-col
-                class="pt-2 pb-1 mx-auto"
-                v-for="(timetable, i) in ttsInfo.ttsByYear[year]"
-                :key="timetable.adeResources"
-              >
-                <router-link
-                  :to="`/edt/${timetable.numUniv}/${timetable.adeResources}`"
-                  tag="button"
-                >
-                  <v-btn
-                    x-large
-                    :class="{'subtitle-1 pl-12 pr-12': $vuetify.breakpoint.lg || $vuetify.breakpoint.xl}"
-                    :color="colorList[i]"
-                    @click="selectEDT(timetable.adeResources)"
-                    dark
-                  >{{timetable.nameTT}}</v-btn>
+            <v-col :cols="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? '6' : '4'" class="mx-auto" v-for="(year) in ttsInfo.yearList"
+              :key="year">
+              <h2 class="font-weight-regular">Année {{ year }} :</h2>
+              <v-col class="pt-2 pb-1 mx-auto" v-for="(timetable, i) in ttsInfo.ttsByYear[year]" :key="timetable.adeResources">
+                <router-link :to="`/edt/${timetable.numUniv}/${timetable.adeResources}`" tag="button">
+                  <v-btn x-large :class="{ 'subtitle-1 pl-12 pr-12': $vuetify.breakpoint.lg || $vuetify.breakpoint.xl }" :color="colorList[i]"
+                    @click="selectEDT(timetable.adeResources)" dark>{{ timetable.nameTT }}</v-btn>
                 </router-link>
               </v-col>
             </v-col>
@@ -43,14 +29,10 @@
       </v-col>
     </v-row>
 
-    <v-col
-      class="text-center mx-auto mb-5"
-      :cols="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? '': '6'"
-    >
-      <v-btn
-        :class="{'subtitle-1 pl-12 pr-12': $vuetify.breakpoint.lg || $vuetify.breakpoint.xl}"
-        @click="requestPage('SelectUniv')"
-      >Retour à la sélection de l'établissement</v-btn>
+    <v-col class="text-center mx-auto mb-5" :cols="$vuetify.breakpoint.xs || $vuetify.breakpoint.sm ? '' : '6'">
+      <v-btn :class="{ 'subtitle-1 pl-12 pr-12': $vuetify.breakpoint.lg || $vuetify.breakpoint.xl }" @click="requestPage('SelectUniv')">
+        Retour à la sélection de l'établissement
+      </v-btn>
     </v-col>
   </v-container>
 </template>

8f33ce821480703f2e953f8cdbf5f8a3b64fcde1
Antonin Huaut <me@antoninhuaut.fr>
2024-10-14 18:30:18 +0200

    fix: Reset calendar date when loading calendar page

--- src/views/Timetable.vue
+++ src/views/Timetable.vue
@@ -8,13 +8,16 @@
 <script lang="ts" setup>
 import TimetableNavigator from "@/components/Timetable/TimetableNavigator.vue";
 import TimetableViewer from "@/components/Timetable/TimetableViewer.vue";
+import { useDateHelper } from "@/hooks/useDateHelper";
 import { usePageTitle } from "@/hooks/usePageTitle";
 import { useTimetable } from "@/hooks/useTimetable";
-import { useAppStore } from "@/store/";
-import { watchEffect } from "vue";
+import { useAppStore, useTimetableViewStore } from "@/store/";
+import { onMounted, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 
 const appStore = useAppStore();
+const dateHelper = useDateHelper();
+const timetableViewStore = useTimetableViewStore();
 const route = useRoute();
 const { adeResources, numUniv } = route.params;
 if (adeResources && numUniv && !Number.isNaN(+adeResources) && !Number.isNaN(+numUniv)) {
@@ -27,4 +30,10 @@ if (adeResources && numUniv && !Number.isNaN(+adeResources) && !Number.isNaN(+nu
 const timetableData = useTimetable();
 const { setPageTitle } = usePageTitle();
 watchEffect(() => setPageTitle(timetableData.nameTT.value === "?" ? "Emploi du temps" : timetableData.nameTT.value));
+
+onMounted(() => {
+    timetableViewStore.$patch({
+        calDate: dateHelper.skipWeekend(new Date(), "next"),
+    });
+});
 </script>

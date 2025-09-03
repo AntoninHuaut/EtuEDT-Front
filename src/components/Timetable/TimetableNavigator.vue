492020a64ea74101c567a15e76191a7f2285ebe4
AntoninHuaut <me@antoninhuaut.fr>
2025-09-03 23:10:23 +0200

    feat: Added current month in timetable view

--- src/components/Timetable/TimetableNavigator.vue
+++ src/components/Timetable/TimetableNavigator.vue
@@ -1,32 +1,40 @@
 <template>
   <v-row justify="space-between" class="align-center">
     <v-col :cols="xs ? '12' : 'auto'" class="d-flex justify-center">
-      <v-tooltip text="💡 [Espace]" open-delay="350">
-        <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" variant="outlined" density="default" :size="getBtnSize()" class="mr-3"
-            @click="() => setDate(null, 'today')">
-            Aujourd'hui
-          </v-btn>
-        </template>
-      </v-tooltip>
+      <v-row justify="space-between" class="align-center px-3 pl-3">
+        <v-col :cols="xs ? '12' : 'auto'" class="d-flex justify-center px-0 py-0">
+          <v-tooltip text="💡 [Espace]" open-delay="350">
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" variant="outlined" density="default" :size="getBtnSize()" class="mr-3"
+                @click="() => setDate(null, 'today')">
+                Aujourd'hui
+              </v-btn>
+            </template>
+          </v-tooltip>
 
-      <p id="timetableName" :class="xs ? 'mx-0' : 'text-h6 mx-2'">{{ timetableData.nameTT }}</p>
+          <p id="timetableName" :class="xs ? 'mx-0' : 'text-h6 mx-2'">{{ timetableData.nameTT }}</p>
 
-      <v-tooltip text="💡 [Flèche/swipe gauche]" open-delay="350">
-        <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" variant="text" density="comfortable" :size="getBtnSize()" icon="mdi-chevron-left"
-            @click="() => setDate(null, 'prev')" />
-        </template>
-      </v-tooltip>
-      <v-tooltip text="💡 [Flèche/swipe droit]" open-delay="350">
-        <template v-slot:activator="{ props }">
-          <v-btn v-bind="props" variant="text" density="comfortable" :size="getBtnSize()" icon="mdi-chevron-right"
-            @click="() => setDate(null, 'next')" />
-        </template>
-      </v-tooltip>
+          <v-tooltip text="💡 [Flèche/swipe gauche]" open-delay="350">
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" variant="text" density="comfortable" :size="getBtnSize()" icon="mdi-chevron-left"
+                @click="() => setDate(null, 'prev')" />
+            </template>
+          </v-tooltip>
+          <v-tooltip text="💡 [Flèche/swipe droit]" open-delay="350">
+            <template v-slot:activator="{ props }">
+              <v-btn v-bind="props" variant="text" density="comfortable" :size="getBtnSize()" icon="mdi-chevron-right"
+                @click="() => setDate(null, 'next')" />
+            </template>
+          </v-tooltip>
+        </v-col>
+
+        <v-col :cols="xs ? '12' : 'auto'" class="d-flex justify-center px-0 py-1">
+          <p :class="xs ? 'mx-0' : 'text-h6 mx-2'"> {{ getDisplayedDate(timetableViewStore.calDate) }}</p>
+        </v-col>
+      </v-row>
     </v-col>
 
-    <v-col :cols="xs ? '12' : 'auto'" :class="'d-flex justify-center ' + (xs ? 'mt-0 pt-0' : '')">
+    <v-col :cols="xs ? '12' : 'auto'" :class="'d-flex justify-center ' + (xs ? 'mt-0 pt-0' : 'pl-0 pr-3 py-2')">
       <v-tooltip text="💡 [W]" open-delay="350">
         <template v-slot:activator="{ props }">
           <v-btn-toggle v-bind="props" v-model="timetableViewStore.viewMode" variant="text" color="blue accent-2" density="compact"
@@ -57,60 +65,69 @@ const { xs } = useDisplay();
 const { isSwiping, direction } = useSwipe(document.body);
 
 function getBtnSize() {
-    return xs.value ? "small" : undefined;
+  return xs.value ? "small" : undefined;
 }
 
 function setDate(evt: KeyboardEvent | null, navigationType: "prev" | "today" | "next") {
-    evt?.preventDefault();
+  evt?.preventDefault();
 
-    let newCalDate: Date;
-    if (navigationType === "today") {
-        newCalDate = new Date();
-    } else {
-        const invertIfPrev = navigationType === "prev" ? -1 : 1;
+  let newCalDate: Date;
+  if (navigationType === "today") {
+    newCalDate = new Date();
+  } else {
+    const invertIfPrev = navigationType === "prev" ? -1 : 1;
 
-        switch (timetableViewStore.viewMode) {
-            case "month-grid":
-                newCalDate = adapter.addMonths(timetableViewStore.calDate, 1 * invertIfPrev) as Date;
-                break;
-            case "week":
-                newCalDate = adapter.addDays(timetableViewStore.calDate, 7 * invertIfPrev) as Date;
-                break;
-            case "day":
-                newCalDate = adapter.addDays(timetableViewStore.calDate, 1 * invertIfPrev) as Date;
-                break;
-            default:
-                return;
-        }
+    switch (timetableViewStore.viewMode) {
+      case "month-grid":
+        newCalDate = adapter.addMonths(timetableViewStore.calDate, 1 * invertIfPrev) as Date;
+        break;
+      case "week":
+        newCalDate = adapter.addDays(timetableViewStore.calDate, 7 * invertIfPrev) as Date;
+        break;
+      case "day":
+        newCalDate = adapter.addDays(timetableViewStore.calDate, 1 * invertIfPrev) as Date;
+        break;
+      default:
+        return;
     }
+  }
 
-    newCalDate = dateHelper.skipWeekend(newCalDate, navigationType);
-    timetableViewStore.$patch({ calDate: newCalDate });
+  newCalDate = dateHelper.skipWeekend(newCalDate, navigationType);
+  timetableViewStore.$patch({ calDate: newCalDate });
+}
+
+function getDisplayedDate(date: Date) {
+  return new Intl.DateTimeFormat(
+    navigator.language,
+    { month: "long", year: "numeric" }
+  )
+    .format(date)
+    .replace(/^\w/, c => c.toUpperCase());
 }
 
 function setView(view: "day" | "week" | "month-grid") {
-    timetableViewStore.$patch({ viewMode: view });
+  timetableViewStore.$patch({ viewMode: view });
 }
 
 watch(isSwiping, (isSwiping) => {
-    if (isSwiping) {
-        if (direction.value === "left") setDate(null, "next");
-        else if (direction.value === "right") setDate(null, "prev");
-    }
+  if (isSwiping) {
+    if (direction.value === "left") setDate(null, "next");
+    else if (direction.value === "right") setDate(null, "prev");
+  }
 });
 
 onKeyStroke(["w", "W"], (e) => {
-    switch (timetableViewStore.viewMode) {
-        case "day":
-            setView("week");
-            break;
-        case "week":
-            setView("month-grid");
-            break;
-        case "month-grid":
-            setView("day");
-            break;
-    }
+  switch (timetableViewStore.viewMode) {
+    case "day":
+      setView("week");
+      break;
+    case "week":
+      setView("month-grid");
+      break;
+    case "month-grid":
+      setView("day");
+      break;
+  }
 });
 
 onKeyStroke("ArrowLeft", (e) => setDate(e, "prev"));
